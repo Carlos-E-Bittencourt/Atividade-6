c21d366b5158b3a9421c7bcbd68654082236a36d
Carlos Bittencourt <carlos_bittencourt2@estudante.sesisenai.org.br>
2025-08-22 12:51:00 -0300

    UPDATE - 'create.php' dos jogadores feito

--- public/jogadores/create.php
+++ public/jogadores/create.php
@@ -1,7 +1,23 @@
 <?php
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+include '../db.php';
 
+$sql = "SELECT * FROM times";
+
+$result = $conn->query($sql);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $nome = $_POST['nome'];
+    $posi = $_POST['positions'];
+    $num_cami = $_POST['num_cami'];
+    $time = $_POST['times'];
+
+    $sql = "INSERT INTO jogadores (nome, posicao, numero_camisa, time_id) VALUES ('$nome', '$posi', '$num_cami', '$time')";
+
+    if($conn ->query($sql) === true)
+        {echo"Jogador criado com sucesso!<a href='read.php'>Ver jogadores</a>";}
+    else{echo "Erro: " . $conn->error;}
+    $conn->close();
 }
 ?>
 <!DOCTYPE html>
@@ -11,14 +27,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="../../styles/style.css">
     <title>Inserir Jogador no Banco</title>
 </head>
 <body>
     
-    <form action="POST">
+    <form action="" method="POST">
         <label>Nome: </label>
         <input type="text" name="nome">
+        <br>
         <label>Posição: </label>
         <select name="positions" id="posi-select">
             <option value="" selected disabled>Selecione...</option>
@@ -27,12 +44,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <option value="MEI">MEI</option>
             <option value="ATA">ATA</option>
         </select>
+        <br>
         <label>Número da Camisa: </label>
         <input type="text" name="num_cami">
+        <br>
         <label>Time: </label>
         <select name="times" id="time-select">
             <option value="" selected disabled>Selecione...</option>
+            <?php
+            if($result->num_rows > 0){
+                while($row = $result->fetch_assoc()){
+                    echo "<option value='" . $row['id'] . "'>" . $row['nome'] . "</option>";
+                }
+            }
+            ?>
         </select>
+        <br>
+        <input type="submit" value="Criar">
+        <input type="button" value="Cancelar" onclick="window.location.href='read.php'">
     </form>
 
 </body>
